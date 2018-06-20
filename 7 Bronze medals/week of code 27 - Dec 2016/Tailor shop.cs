d809ff83844d63792ed47d2a8346191856a367e5
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-20 12:38:18 -0700

    check in source code

---
+++ 7 Bronze medals/week of code 27 - Dec 2016/Tailor shop.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TailorShop();
+            //RunSampleTestCase1();
+        }
+
+        private static void RunSampleTestCase1()
+        {
+            long sum = CalMinimumSumWithDistinctNoButtons(3, 2, new int[] { 4, 6, 6 });
+            Debug.Assert(sum == 9);
+        }
+
+        private static void TailorShop()
+        {
+            int[] data = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int colors = data[0];
+            int cost = data[1];
+
+            int[] minimumCosts = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Console.WriteLine(CalMinimumSumWithDistinctNoButtons(colors, cost, minimumCosts));
+        }
+        /*
+         * spec: 
+         * 1. Distinct number of buttons of a certain color
+         * each color 
+         */
+        private static long CalMinimumSumWithDistinctNoButtons(
+            int colors,
+            int cost,
+            int[] minimumCosts
+            )
+        {
+            Array.Sort(minimumCosts);
+
+            ConvertToNumber(minimumCosts, cost);
+
+            Dictionary<int, int> countingData = new Dictionary<int, int>();
+
+            countingData = Count(minimumCosts);
+
+            int[] keyData = countingData.Keys.ToArray();
+            Array.Sort(keyData);
+
+            long sum = 0;
+            int needToFill = 0;
+            bool isFirst = true;
+            int prev = -1;
+            foreach (int value in keyData)
+            {
+                int count = countingData[value];
+
+                // take care some duplicates
+                int start = prev + 1;
+                int end = value - 1;
+                int gap = end - start;
+
+                if (needToFill > 0 &&
+                    gap > 0 &&
+                    !isFirst)
+                {
+
+
+                    if (needToFill <= gap)
+                    {
+                        int end2 = start + needToFill - 1;
+                        sum += SumValue(start, end2);
+                        needToFill = 0;
+                    }
+                    else
+                    {
+                        sum += SumValue(prev, value);
+                        needToFill -= gap;
+                    }
+                }
+
+                sum += value;
+                prev = value;
+                if (count > 1)
+                {
+                    needToFill += count - 1;
+                }
+
+                if (isFirst)
+                    isFirst = false;
+            }
+
+            //edge case
+            if (needToFill > 0)
+                sum += SumValue(prev + 1, prev + needToFill);
+
+            // add the sum of numbers
+            return sum;
+        }
+
+        private static void ConvertToNumber(int[] minimumCosts, int cost)
+        {
+            for (int i = 0; i < minimumCosts.Length; i++)
+            {
+                int value = minimumCosts[i];
+                int number = value / cost;
+                if (number * cost < value)
+                    number++;
+
+                minimumCosts[i] = number;
+            }
+        }
+
+        private static long SumValue(int start, int end)
+        {
+            long sum = (long)start + (long)end;
+
+            return sum * (end - start + 1) / 2;
+        }
+
+        private static Dictionary<int, int> Count(int[] minimumCosts)
+        {
+            Dictionary<int, int> countingData = new Dictionary<int, int>();
+
+            foreach (int value in minimumCosts)
+            {
+                if (countingData.ContainsKey(value))
+                {
+                    countingData[value]++;
+                }
+                else
+                {
+                    countingData[value] = 1;
+                }
+            }
+
+            return countingData;
+        }
+    }
+}
---
+++ 7 Bronze medals/week of code 27 - Dec 2016/zero move nim.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zeroMoveNim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For hackerRank
+            RunHackerRank();
+
+            // For my testing 
+            // RunSampleTestCases(); 
+        }
+
+        private static void RunHackerRank()
+        {
+            IList<char> results = ProcessZeroMoveGame();
+            Output(results);
+
+        }
+
+        private static void Output(IList<char> data)
+        {
+            foreach (char c in data)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        private static void RunSampleTestCases()
+        {
+            // First player 'W',   second player 'L'
+
+            //Debug.Assert( ProcessZerorMoveNimGame(2, new int[] { 2, 2 }) == 'L');     // Second player
+            //Debug.Assert( ProcessZerorMoveNimGame(2, new int[] { 1, 2 }) == 'W');     // go to first one
+            //Debug.Assert( ProcessZerorMoveNimGame(3, new int[] { 1, 2, 3 }) == 'W');  // go to first  'W'
+            //Debug.Assert(ProcessZerorMoveNimGame(3, new int[] { 2, 2, 4 }) == 'W');   // go to first
+            Debug.Assert(ProcessZerorMoveNimGame_usingNimSum(3, new int[] { 2, 3, 4 }) == 'L');   // go to second
+        }
+
+        private static void RunSampleTestCase1()
+        {
+            int result = ProcessZerorMoveNimGame_usingNimSum(2, new int[] { 2, 2 });
+        }
+
+        private static IList<char> ProcessZeroMoveGame()
+        {
+            IList<char> results = new List<char>();
+
+            int nQueries = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nQueries; i++)
+            {
+                int nPiles = Convert.ToInt32(Console.ReadLine());
+                int[] piles = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                results.Add(ProcessZerorMoveNimGame_usingNimSum(nPiles, piles));
+            }
+
+            return results;
+        }
+
+
+        /*
+         * ProcessZerorMoveNimGame_usingNimSum
+         * https://en.wikipedia.org/wiki/Nim
+         * 
+         */
+        private static char ProcessZerorMoveNimGame_usingNimSum(int nPiles, int[] piles)
+        {
+            int nimSum = 0;
+
+            for (int i = 0; i < piles.Length; i++)
+            {
+                nimSum = nimSum ^ piles[i];
+            }
+
+            int countZeroMove = nPiles;
+
+            if (countZeroMove % 2 == 0)
+            {
+                return (nimSum == 0) ? 'L' : 'W';
+            }
+            else
+            {
+                return (nimSum == 0) ? 'W' : 'L';
+            }
+        }
+    }
+}
