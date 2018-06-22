d36a3261c371ed9bacf828da1af53f5babc5ed10
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-22 11:15:22 -0700

    check in source code

---
+++ practice/data structure/Cycle Detection.cpp
@@ -0,0 +1,33 @@
+﻿/*
+  Detect loop in a linked list 
+  List could be empty also
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+int HasCycle(Node* head)
+{
+   // Complete this function
+   // Do not write the main method
+   if(head == NULL || head->next== NULL || head->next->next==NULL)
+       return 0; 
+   
+   Node* slow = head->next; 
+   Node* fast = head->next->next; 
+    
+   while(slow!=NULL && fast !=NULL) 
+   {
+      if(slow==fast)    
+          return 1; 
+      slow=slow->next; 
+       
+      if(fast->next == NULL)
+          return 0; 
+      fast = fast->next->next; 
+   }
+    
+   return 0; 
+}
