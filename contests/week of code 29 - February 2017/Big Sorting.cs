ae9748e2d1129503bed92ce8da0e9f20c4fc704a
Jianmin Chen <jianminchen.fl@gmail.com>
2018-06-22 14:19:26 -0700

    check in the source code

---
+++ contests/week of code 29 - February 2017/Big Sorting.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigSorting_B
+{
+    /// <summary>
+    /// https://www.hackerrank.com/contests/w29/challenges/big-sorting
+    /// </summary>
+    public class StringComparer : IComparer<string>
+    {
+        public int Compare(string s1, string s2)
+        {
+            int length1 = s1.Length;
+            int length2 = s2.Length;
+
+            if (length1 != length2)
+            {
+                if (length1 > length2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            int start = 0;
+            while (start < length1)
+            {
+                if (s1[start] != s2[start])
+                {
+                    if (s1[start] > s2[start])
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+
+                start++;
+            }
+
+            return 0;
+        }
+    }
+
+    class BigSorting_B
+    {
+        static void Main(string[] args)
+        {
+            var unsorted = ProcessInput();
+            // var unsorted = new string[] {"314567","1","3","10","3","5" };
+            // var sorted  = new SortedSet<string>(unsorted, new StringComparer());
+            Array.Sort(unsorted, new StringComparer());
+
+            foreach (string s in unsorted)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        public static string[] ProcessInput()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] unsorted = new string[n];
+            for (int unsorted_i = 0; unsorted_i < n; unsorted_i++)
+            {
+                unsorted[unsorted_i] = Console.ReadLine();
+            }
+
+            return unsorted;
+        }
+    }
+}
---
+++ contests/week of code 29 - February 2017/Day of the programmer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfProgrammer
+{
+    /// <summary>
+    /// https://www.hackerrank.com/contests/w29/challenges/day-of-the-programmer/problem
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(CalculateDayOfProgrammer(year));
+        }
+
+        public static string CalculateDayOfProgrammer(int year)
+        {
+            int start = 1700;
+
+            bool isJulianCalendar = year >= start && year <= 1917;
+            bool isGregorianCalendar = year >= 1919;
+            int[] daysInFirst8Months = new int[] { 31, 28, 31, 30, 31, 30, 31, 31 };
+
+            int offDaysInFeb = 0;
+            if (year == 1998)
+            {
+                offDaysInFeb = 13;
+            }
+
+            if (isJulianCalendar && year % 4 == 0)
+            {
+                daysInFirst8Months[1] = 29;
+            }
+
+            if (isGregorianCalendar && (year % 400 == 0 || (year % 4 == 0 && year % 100 > 0)))
+            {
+                daysInFirst8Months[1] = 29;
+            }
+
+            daysInFirst8Months[1] -= offDaysInFeb;
+
+            int theDayOfSept = 256 - daysInFirst8Months.Sum();
+
+            return theDayOfSept + ".09." + year;
+        }
+    }
+}
