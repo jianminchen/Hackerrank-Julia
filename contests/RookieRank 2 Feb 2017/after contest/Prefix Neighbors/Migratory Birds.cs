d696a4f682d50bbc6c0d14455dc2d3bcf80f2792
Jianmin Chen <jianminchen.fl@gmail.com>
2018-07-05 16:52:41 -0700

    check in the source code

---
+++ contests/RookieRank 2 Feb 2017/after contest/Prefix Neighbors/Migratory Birds.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ids = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Console.WriteLine(CalculateMaximum(ids));
+        }
+
+        public static int CalculateMaximum(int[] ids)
+        {
+            int n = ids.Length;
+
+            int[] idCounts = new int[5];
+
+            for (int i = 0; i < n; i++)
+            {
+                int id = ids[i] - 1;
+                idCounts[id]++;
+            }
+
+            int maxValue = idCounts[0];
+            int idWithMaxValue = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                if (idCounts[i] > maxValue)
+                {
+                    idWithMaxValue = i;
+                    maxValue = idCounts[i];
+                }
+            }
+
+            return idWithMaxValue + 1;
+        }
+    }
+}
