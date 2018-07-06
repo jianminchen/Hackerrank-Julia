4c5f901ac7748c63919ca387685b5991fd41b8f3
Jianmin Chen <jianminchen.fl@gmail.com>
2018-07-05 17:01:57 -0700

    check in source code

---
+++ contests/RookieRank 3 May 2017/After contest/culture conference/Culture Conference.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultureConference_studyCode
+{
+    /// <summary>
+    /// culture conference 
+    /// https://www.hackerrank.com/contests/rookierank-3/challenges/culture-conference
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var edges = new int[n - 1][];
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                string[] data = Console.ReadLine().Split(' ');
+                edges[i] = Array.ConvertAll(data, Int32.Parse);
+            }
+
+            var result = GetMinimumNumberOfEmployees(n, edges);
+
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Very straightforward idea to solve the problem. 
+        /// time complexity  - each node is checked once
+        /// space complexity - 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="edges"></param>
+        /// <returns></returns>
+        public static int GetMinimumNumberOfEmployees(int n, int[][] edges)
+        {
+            var burntList = new int[n];
+            var supervisor = new int[n];
+            var subordinates = new HashSet<int>[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                subordinates[i] = new HashSet<int>();
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                int supervisorId, burntOut;
+                supervisorId = edges[i - 1][0];
+                burntOut = edges[i - 1][1];
+
+                supervisor[i] = supervisorId;
+
+                subordinates[supervisorId].Add(i);
+
+                burntList[i] = (burntOut == 0) ? 0 : 1;
+            }
+
+            int count = 0;
+            bool checkFlag;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                var current = i;
+
+                // loop over all subordinates
+                checkFlag = false;
+                foreach (var item in subordinates[current])
+                {
+                    // check if there is a burnt out subordinate                    
+                    if (burntList[item] == 0)
+                    {
+                        // if yes, make this green and all subordinates and superior green
+                        checkFlag = true;
+
+                        // choose to send current employee to the conference, 
+                        // then its supervisor and its children will not burn out. 
+                        burntList[current] = 1;
+                        burntList[item] = 1;
+                        burntList[supervisor[current]] = 1;
+                    }
+                }
+
+                if (checkFlag)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ contests/RookieRank 3 May 2017/Culture Conference.cs
@@ -0,0 +1,312 @@
+﻿#if DEBUG
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            ProcessInput();
+            //RunUnionFind(); 
+            //RunUnionFindTestCase();
+        }
+
+        public static void ProcessInput()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            // Information for employees 1 through n - 1:
+            // The first value is the employee's supervisor ID
+            // The second value is the employee's burnout status (0 is burned out, 1 is not)
+            int[][] e = new int[n - 1][];
+            for (int e_i = 0; e_i < n - 1; e_i++)
+            {
+                string[] e_temp = Console.ReadLine().Split(' ');
+                e[e_i] = Array.ConvertAll(e_temp, Int32.Parse);
+            }
+
+            long minimumEmployees = RunUnionFind(e);
+
+            Console.WriteLine(minimumEmployees);
+        }
+
+        /*
+         * Calculate the value of friendships
+         */
+        public static long RunUnionFind(int[][] edges)
+        {
+            var unionFind = new UnionFind();
+
+            int count = 0;
+            int index = 1;
+            bool ceoFound = false;
+            foreach (var edge in edges)
+            {
+                var left = index;
+                var right = edge[0];
+                var burnout = edge[1];
+
+                if (burnout == 1)
+                {
+                    continue;
+                }
+
+                if (right == 0)
+                {
+                    ceoFound = true;
+                }
+
+                if (unionFind.IsSameGroup(left, right))
+                {
+                    count++;
+                    continue;
+                }
+
+                unionFind.Unite(right, left);
+
+                index++;
+            }
+            var bigThanOne = 0;
+            var groups = unionFind.GetGroupsSpecial(ref bigThanOne).Where(v => v != 0).Select(v => v + 1).ToList();
+
+            var original = groups.Sum();
+            return (ceoFound) ? (original - 1) : original;
+        }
+
+        /*
+         * Calculate the value of friendships
+         */
+        public static void RunUnionFindTestCase()
+        {
+            var unionFind = new UnionFind();
+            var edges = new int[4][];
+            edges[0] = new int[] { 0, 0 };
+            edges[1] = new int[] { 1, 0 };
+            edges[2] = new int[] { 2, 0 };
+            edges[3] = new int[] { 2, 0 };
+
+
+            int count = 0;
+            int index = 1;
+            foreach (var edge in edges)
+            {
+                var left = index;
+                var right = edge[0];
+                var burnout = edge[1];
+
+
+                if (burnout == 1)
+                {
+                    continue;
+                }
+
+                if (unionFind.IsSameGroup(left, right))
+                {
+                    count++;
+                    continue;
+                }
+
+                unionFind.Unite(left, right);
+
+                index++;
+            }
+
+            var bigThanOne = 0;
+            var groups = unionFind.GetGroupsSpecial(ref bigThanOne).Where(v => v != 0).Select(v => v + 1).ToList();
+
+            groups.Sort();
+            var result = groups.Sum() - bigThanOne;
+        }
+    }
+
+    /*
+     * Class UnionFind 
+     * 
+     */
+    public class UnionFind
+    {
+        private class Node
+        {
+            public Node Parent { get; set; }
+            public int Rank { get; set; }
+            public int Id { get; set; }
+
+            public HashSet<Node> Children;
+
+            public Node()
+            {
+                Children = new HashSet<Node>();
+            }
+        }
+
+        private Dictionary<object, Node> _dict = new Dictionary<object, Node>();
+
+        private Node Root(object data)
+        {
+            if (!_dict.ContainsKey(data))
+            {
+                var node = new Node();
+                _dict.Add(data, node);
+                return node;
+            }
+            else
+            {
+                var node = _dict[data];
+                return Find(node);
+            }
+        }
+
+        private Node Find(Node node)
+        {
+            if (node.Parent == null)
+            {
+                return node;
+            }
+
+            return node.Parent = Find(node.Parent);
+        }
+
+        public void Unite(IComparable x, IComparable y)
+        {
+            var xRoot = Root(x);
+            var yRoot = Root(y);
+            if (xRoot == yRoot) return;
+
+            if (xRoot.Rank < yRoot.Rank)
+            {
+                ChangeParent(yRoot, xRoot);
+            }
+            else
+            {
+                ChangeParent(xRoot, yRoot);
+
+                if (xRoot.Rank == yRoot.Rank)
+                {
+                    xRoot.Rank++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="childRoot"></param>
+        void ChangeParent(Node parent, Node childRoot)
+        {
+            if (childRoot.Parent != null)
+            {
+                childRoot.Parent.Children.Remove(childRoot);
+            }
+
+            childRoot.Parent = parent;
+            childRoot.Parent.Children.Add(childRoot);
+
+            foreach (var child in childRoot.Children.ToList())
+            {
+                ChangeParent(parent, child);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool IsSameGroup(IComparable x, IComparable y)
+        {
+            return Root(x) == Root(y);
+        }
+
+        /// <summary>
+        /// code review: May 6 2017
+        /// </summary>
+        /// <returns></returns>
+        public List<long> GetGroups()
+        {
+            var groups = new List<long>();
+
+            foreach (var group in _dict.Values.Where(d => d.Parent == null))
+            {
+                groups.Add(group.Children.Count());
+            }
+
+            return groups;
+        }
+
+        public List<long> GetGroupsSpecial(ref int bigThanOne)
+        {
+            var groups = new List<long>();
+
+            int index = 0;
+            foreach (var group in _dict.Values.Where(d => d.Parent == null))
+            {
+                var number = group.Children.Count();
+                if (number == 1)
+                {
+                    groups.Add(1);
+                }
+                else
+                {
+                    int count = saveParentChildren(group);
+                    groups.Add(number - count);
+                    index++;
+                }
+            }
+
+            return groups;
+        }
+
+        private int saveParentChildren(Node node)
+        {
+            if (node == null || node.Children.Count == 0) return 0;
+
+            var number = node.Children.Count;
+            int count = node.Children.Count - 1;
+
+            if (number == 1) count = 1;
+            foreach (var child in node.Children)
+            {
+                count += saveParentChildren(child);
+            }
+
+            return count;
+        }
+    }
+
+#if DEBUG
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void Tests()
+        {
+           /*
+            var n = 5;
+            var m = 4;
+            var friends = new List<Tuple<int, int>>
+            {
+                new Tuple<int, int>(1, 2),
+                new Tuple<int, int>(3, 2),
+                new Tuple<int, int>(4, 2),
+                new Tuple<int, int>(4, 3),
+            };
+
+            Assert.AreEqual(32, Program.CalculateValueOfFriendships(n, friends));          
+            * */
+        }       
+    }
+
+#endif
+}
